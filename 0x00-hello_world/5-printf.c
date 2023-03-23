bac8069f1de6c5a464bab17b9362c5980b388780
Oivia-Adel <Oliviaadelattya@gmail.com>
2023-03-23 14:47:59 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Starts program execution
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
